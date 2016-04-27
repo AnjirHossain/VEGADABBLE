0f846c5ed37226730b8222c426ff48caff93aac6
Anjir Hossain <anjirhossain@gmail.com>
2016-04-27 15:18:07 -0700

    app listens

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ let routes = require('./routes/index')(app);
 // view engine setup
 app.set('views', path.join(__dirname, 'app/views'));
 app.set('view engine', 'jade');
+app.set('port', process.env.PORT || 3000);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -54,5 +55,7 @@ app.use((err, req, res, next) => {
     error: {}
   });
 });
+
+app.listen(app.get('port'));
 
 module.exports = app;
