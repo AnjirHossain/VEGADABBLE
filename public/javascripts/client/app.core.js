8c2f2ffc2a05cab07a68135a7513a7466afb5094
Anjir Hossain <anjirhossain@gmail.com>
2016-05-02 16:52:16 -0700

    Converted es6 to es6

--- public/javascripts/client/app.core.js
+++ public/javascripts/client/app.core.js
@@ -1,65 +1,71 @@
+'use strict';
+
 {
-  'use strict';
+  (function () {
 
-  // state/variable cache
-  let endPoint,
-      ajax,
-      apiContents,
-      vegaView;
+    // wire up API fetching
 
-  init();
+    var init = function init() {
+      endPoint = 'http://mysafeinfo.com/api/data?list=fatalaccidentsbycounty_2013_wa&format=json&select=yr,st,cd,cty,cnt,sp,ttl&alias=yr=year,st=state_code,cd=county_code,cty=county_name,cnt=fatal_accidents,sp=speeding,ttl=total_fatalities';
+      ajax = new XMLHttpRequest();
+      ajax.onload = mapDataToScreen; // normalizes response text for vega-lite visualization
+      ajax.open('GET', endPoint, true);
+      ajax.send();
+    };
 
-  // wire up API fetching
-  function init() {
-    endPoint = 'http://mysafeinfo.com/api/data?list=fatalaccidentsbycounty_2013_wa&format=json&select=yr,st,cd,cty,cnt,sp,ttl&alias=yr=year,st=state_code,cd=county_code,cty=county_name,cnt=fatal_accidents,sp=speeding,ttl=total_fatalities';
-    ajax = new XMLHttpRequest();
-    ajax.onload = mapDataToScreen; // normalizes response text for vega-lite visualization
-    ajax.open('GET', endPoint, true);
-    ajax.send();
-  }
+    // maps contents of API to the view via vega-lite general visualizations
 
-  // maps contents of API to the view via vega-lite general visualizations
-  function mapDataToScreen() {
-    apiContents = JSON.parse(this.responseText);
 
-    // conduct vega wireup here
-    let specs = {
-      "data": {
-        "values": apiContents // apiContents expected to be in array
-      },
-      "mark": "bar",
-      "encoding": {
-        "x": {
-          "field": "county_name",
-          "type": "nominal",
-          "axis": {
-            "title": "County"
+    var mapDataToScreen = function mapDataToScreen() {
+      apiContents = JSON.parse(this.responseText);
+
+      // conduct vega wireup here
+      var specs = {
+        "data": {
+          "values": apiContents // apiContents expected to be in array
+        },
+        "mark": "bar",
+        "encoding": {
+          "x": {
+            "field": "county_name",
+            "type": "nominal",
+            "axis": {
+              "title": "County"
+            }
+          },
+          "y": {
+            "field": "fatal_accidents",
+            "type": "quantitative",
+            "axis": {
+              "title": "Deaths"
+            }
           }
         },
-        "y": {
-          "field": "fatal_accidents",
-          "type": "quantitative",
-          "axis": {
-            "title": "Deaths"
+        "config": {
+          "mark": {
+            "filled": true,
+            "color": "#23E363"
           }
         }
-      },
-      "config": {
-        "mark": {
-          "filled": true,
-          "color": "#23E363"
-        }
-      }
-    }
+      };
 
-    let embeddedSpecs = {
-      mode: "vega-lite",
-      spec: specs
-    }
+      var embeddedSpecs = {
+        mode: "vega-lite",
+        spec: specs
+      };
 
-    vg.embed("#vgtarget", embeddedSpecs, (error, result) => {
-      // result.views ready for usage
-      vegaView = result.view;
-    });
-  }
+      vg.embed("#vgtarget", embeddedSpecs, function (error, result) {
+        // result.views ready for usage
+        vegaView = result.view;
+      });
+    };
+
+    // state/variable cache
+    var endPoint = void 0,
+        ajax = void 0,
+        apiContents = void 0,
+        vegaView = void 0;
+
+    init();
+  })();
 }
